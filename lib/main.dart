57b8464891cfa015861a59d3f964c7eb698504c8
David <davidpineda.co@gmail.com>
2021-02-06 09:08:04 +0000

    convert MyHomePage widget in StatelessWidget (don't need for setState anymore)

--- lib/main.dart
+++ lib/main.dart
@@ -18,29 +18,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  final int _counter = 0;
 
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  MyHomePage({this.title});
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  void _incrementCounter() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
